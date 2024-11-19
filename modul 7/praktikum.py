344889490f774985314d66493aac1980650a6ec1
Arzhacode <240441100062@student.trunojoyo.ac.id>
2024-11-19 18:48:31 +0700

    240441100062_ArzhaDwiAnandaAnharWijayaAsprakKaAr'Raffi

---
+++ modul 7/praktikum.py
@@ -0,0 +1,40 @@
+# dict = {
+#     "name" : 'Arzha',
+#      'age' : 17,
+#      'class' :'first'
+# }
+# dict['age']=10
+# dict['class']= 'two'
+# dict['sekolah']= 'UTM TERCINTA'
+# # del dict['name']
+# # dict.clear()
+# data_copy = dict.copy()
+# print(data_copy.get('name'))
+# print('name' in data_copy)
+# print('alamat' in data_copy)
+# print(data_copy.keys())
+# data_copy.pop('age')
+
+# print(data_copy)
+# # print ("dict['name']",dict['name'] )
+# # print ("dict['age']",dict['age'] )
+# # print ("dict['class']",dict['class'] )
+# # print ("dict['sekolah']",dict['sekolah'] )
+# print(f"{dict}")
+
+
+
+# #contoh set atau himpunan
+# himpunan = set_a = {1,2,3,4,5}
+# himpunan = set_b = {4,5,6,7,8}
+
+# himpunan = set_b.add(9)
+# print(himpunan)
+
+# # (4,5,6,7,8,9)
+# himpunan = set_a.union(set_b) #union
+# print(set_a)
+# set_a.intersection(set_b) #intersection
+# print(set_a)
+# set_a.difference(set_b) #difference
+# print(set_a)
